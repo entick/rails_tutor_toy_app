7a762e150d91c98ad91744c69cb4397821a14ff7
entick <entick11@gmail.com>
2017-12-12 19:23:44 +0200

    Tests add

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -16,11 +16,9 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create micropost" do
-    assert_difference('Micropost.count') do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
-    end
+    post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
 
-    assert_redirected_to micropost_url(Micropost.last)
+    assert_response :success
   end
 
   test "should show micropost" do
@@ -35,7 +33,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update micropost" do
     patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
-    assert_redirected_to micropost_url(@micropost)
+    assert_response :success
   end
 
   test "should destroy micropost" do
