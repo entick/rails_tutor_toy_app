6095591722e7cffbde869eaf99b4c47bf081e110
entick <entick11@gmail.com>
2017-12-16 17:00:32 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -53,6 +55,12 @@ class User < ApplicationRecord
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
 
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,46 +1,32 @@
-# require 'test_helper'
-#
-# class MicropostsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     @micropost = microposts(:one)
-#   end
-#
-#   test "should get index" do
-#     get microposts_url
-#     assert_response :success
-#   end
-#
-#   test "should get new" do
-#     get new_micropost_url
-#     assert_response :success
-#   end
-#
-#   test "should create micropost" do
-#     post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
-#
-#     assert_response :success
-#   end
-#
-#   test "should show micropost" do
-#     get micropost_url(@micropost)
-#     assert_response :success
-#   end
-#
-#   test "should get edit" do
-#     get edit_micropost_url(@micropost)
-#     assert_response :success
-#   end
-#
-#   test "should update micropost" do
-#     patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
-#     assert_response :success
-#   end
-#
-#   test "should destroy micropost" do
-#     assert_difference('Micropost.count', -1) do
-#       delete micropost_url(@micropost)
-#     end
-#
-#     assert_redirected_to microposts_url
-#   end
-# end
+require 'test_helper'
+
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
+
+end
